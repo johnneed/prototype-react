f506862e056d8a0600719fd51e818e86c7692d2e
John Need <johnneed@johnneed.com>
2020-03-09 12:28:45 -0400

    persistant drawer

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,28 +1,27 @@
 // @flow
-
 import React from "react";
-import "./layout.css";
-import AppBar from "@material-ui/core/AppBar";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Divider from "@material-ui/core/Divider";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import Hidden from "@material-ui/core/Hidden";
-import IconButton from "@material-ui/core/IconButton";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import SettingsIcon from "@material-ui/icons/Settings";
-import MenuIcon from "@material-ui/icons/Menu";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
 import AddCircleOutline from "@material-ui/icons/AddCircleOutline";
 import ListIcon from "@material-ui/icons/List";
 import LogoutIcon from "@material-ui/icons/ExitToApp";
 import SearchIcon from "@material-ui/icons/Search";
 import DashboardIcon from "@material-ui/icons/Dashboard";
-import { Link } from "react-router-dom";
+import SettingsIcon from "@material-ui/icons/Settings";
 import ListItemLink from "../list-item-link";
 
 const drawerWidth = 240;
@@ -31,47 +30,72 @@ const useStyles = makeStyles(theme => ({
     root: {
         display: "flex"
     },
-    drawer: {
-        [theme.breakpoints.up("sm")]: {
-            width: drawerWidth,
-            flexShrink: 0
-        }
-    },
     appBar: {
-        [theme.breakpoints.up("sm")]: {
-            width: `calc(100% - ${ drawerWidth }px)`,
-            marginLeft: drawerWidth
-        }
+        transition: theme.transitions.create(["margin", "width"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        })
+    },
+    appBarShift: {
+        width: `calc(100% - ${ drawerWidth }px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(["margin", "width"], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen
+        })
     },
     menuButton: {
-        marginRight: theme.spacing(2),
-        [theme.breakpoints.up("sm")]: {
-            display: "none"
-        }
+        marginRight: theme.spacing(2)
     },
-    toolbar: theme.mixins.toolbar,
+    hide: {
+        display: "none"
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0
+    },
     drawerPaper: {
         width: drawerWidth
     },
+    drawerHeader: {
+        display: "flex",
+        alignItems: "center",
+        padding: theme.spacing(0, 1),
+        ...theme.mixins.toolbar,
+        justifyContent: "flex-end"
+    },
     content: {
         flexGrow: 1,
-        padding: theme.spacing(3)
+        padding: theme.spacing(3),
+        transition: theme.transitions.create("margin", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        }),
+        marginLeft: -drawerWidth
+    },
+    contentShift: {
+        transition: theme.transitions.create("margin", {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen
+        }),
+        marginLeft: 0
     }
 }));
-
 type PropsType = { children: React$Component<any> };
 
-
 export const Layout = (props: PropsType) => {
-    const { container, children } = props;
+    const { children } = props;
     const classes = useStyles();
     const theme = useTheme();
-    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
+    const handleDrawerOpen = () => {
+        setOpen(true);
     };
 
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
     const reportsMenu = [
         {
@@ -108,82 +132,69 @@ export const Layout = (props: PropsType) => {
         { order: 0, key: "logout", text: "Logout", icon: (<LogoutIcon/>), to: "logout" }
     ];
 
-    const drawer = (
-        <div>
-            <div className={ classes.toolbar }/>
-            <Divider/>
-            <List>
-                { reportsMenu.map((item) => (
-                    <ListItemLink key={ item.key } { ...item } />
-                )) }
-            </List>
-            <Divider/>
-            <List>
-                { userMenu.map((item) => (
-                    <ListItem button key={ item.key }>
-                        <ListItemIcon>{ item.icon }</ListItemIcon>
-                        <ListItemText primary={ item.text }/>
-                    </ListItem>
-                )) }
-            </List>
-        </div>
-    );
 
     return (
         <div className={ classes.root }>
             <CssBaseline/>
-            <AppBar position="fixed" className={ classes.appBar }>
+            <AppBar
+                position="fixed"
+                className={ clsx(classes.appBar, {
+                    [classes.appBarShift]: open
+                }) }
+            >
                 <Toolbar>
                     <IconButton
                         color="inherit"
                         aria-label="open drawer"
+                        onClick={ handleDrawerOpen }
                         edge="start"
-                        onClick={ handleDrawerToggle }
-                        className={ classes.menuButton }
+                        className={ clsx(classes.menuButton, open && classes.hide) }
                     >
                         <MenuIcon/>
                     </IconButton>
                     <Typography variant="h6" noWrap>
-                        ICE CALL CENTER
+                        CALL CENTER
                     </Typography>
                 </Toolbar>
             </AppBar>
-            <nav className={ classes.drawer } aria-label="mailbox folders">
-                <Hidden smUp implementation="css">
-                    <Drawer
-                        container={ container }
-                        variant="temporary"
-                        anchor={ theme.direction === "rtl" ? "right" : "left" }
-                        open={ mobileOpen }
-                        onClose={ handleDrawerToggle }
-                        classes={ {
-                            paper: classes.drawerPaper
-                        } }
-                        ModalProps={ {
-                            keepMounted: true // Better open performance on mobile.
-                        } }
-                    >
-                        { drawer }
-                    </Drawer>
-                </Hidden>
-                <Hidden xsDown implementation="css">
-                    <Drawer
-                        classes={ {
-                            paper: classes.drawerPaper
-                        } }
-                        variant="permanent"
-                        open
-                    >
-                        { drawer }
-                    </Drawer>
-                </Hidden>
-            </nav>
-            <main className={ classes.content }>
-                <div className={ classes.toolbar }/>
+            <Drawer
+                className={ classes.drawer }
+                variant="persistent"
+                anchor="left"
+                open={ open }
+                classes={ {
+                    paper: classes.drawerPaper
+                } }
+            >
+                <div className={ classes.drawerHeader }>
+                    <IconButton onClick={ handleDrawerClose }>
+                        { theme.direction === "ltr" ? <ChevronLeftIcon/> : <ChevronRightIcon/> }
+                    </IconButton>
+                </div>
+                <Divider/>
+                <List>
+                    { reportsMenu.map((item) => (
+                        <ListItemLink key={ item.key } { ...item } />
+                    )) }
+                </List>
+                <Divider/>
+                <List>
+                    { userMenu.map((item) => (
+                        <ListItem button key={ item.key }>
+                            <ListItemIcon>{ item.icon }</ListItemIcon>
+                            <ListItemText primary={ item.text }/>
+                        </ListItem>
+                    )) }
+                </List>
+            </Drawer>
+            <main
+                className={ clsx(classes.content, {
+                    [classes.contentShift]: open
+                }) }
+            >
+                <div className={ classes.drawerHeader }/>
                 { children }
             </main>
         </div>
     );
 };
-
-
