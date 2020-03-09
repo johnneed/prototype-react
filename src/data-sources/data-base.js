35c971608e949ae8ffd5629433581d64247cab49
John Need <johnneed@johnneed.com>
2020-03-09 15:06:31 -0400

    better dummy data

--- src/data-sources/data-base.js
+++ src/data-sources/data-base.js
@@ -1,16 +1,28 @@
 const reports = {
-    1: {
-        dateTime: Date(),
+    report1: {
+        dateTime: Date("2020-01-01"),
         pin: 1234,
         type: "accident",
-        officer: "Jones"
+        officer: "Jones",
+        subject: {firstName: "Ethan", lasName: "Allen"},
+        vehicle: {vehicleColor: "red", vehicleYear: "2020", vehicleMake: "Honda", vehicleModel: "Accord", licensePlateNumber: "BAD 999"}
     },
-    2: {
-        dateTime: Date(),
+    report2: {
+        dateTime: Date("2020-02-19"),
         pin: 3456,
         type: "traffic stop",
-        officer: "Smith"
-    }
+        officer: "Smith",
+        subject: {firstName: "Fred", lasName: "Frederickson"},
+        vehicle: {vehicleYear: "1999", vehicleMake: "Ford", vehicleModel: "Fiesta", licensePlateNumber: "ABC 123"}
+    },
+    report3: {
+        dateTime: Date("2020-02-29"),
+        pin: 1234,
+        type: "traffic stop",
+        officer: "Smith",
+        subject: {firstName: "Ethan", lasName: "Allen"},
+        vehicle: {vehicleColor: "red", vehicleYear: "2020", vehicleMake: "Honda", vehicleModel: "Accord", licensePlateNumber: "BAD 999"}
+    },
 };
 
 // Fake API Call
